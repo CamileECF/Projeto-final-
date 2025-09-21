c461b50d5aca236d227879fdb0779e8bad079a65
Emanoel Alexandre Barbosa Batista <alebbalexandre@gmail.com>
2025-09-20 23:28:44 -0300

    Funcionalidade completa de pedidos e estoque
    Esta classe de Modelo representa a entidade "Medicamento". Ela serve como a estrutura de dados para qualquer produto que pode ser vendido no sistema, armazenando atributos fundamentais como id, nome, preço e quantidade em estoque. É a base para o controle de inventário.

---
+++ ProjetoFinal/src/main/java/model/Medicamento.java
@@ -0,0 +1,73 @@
+package model;
+
+/**
+ * @author Emanoel Alexandri
+ */
+public class Medicamento {
+
+    private long id;
+    private String nome;
+    private String descricao;
+    private String fabricante;
+    private double preco;
+    private int quantidadeEstoque;
+
+    public Medicamento() {
+    }
+
+    public Medicamento(String nome, String descricao, double preco, int quantidadeEstoque) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    // Getters e Setters
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
+
+    public void setQuantidadeEstoque(int quantidadeEstoque) {
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+}
